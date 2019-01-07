e8d3086e11adf42204f952658884d5169d1e7693
mikesuchor <mikesuchor@gmail.com>
2019-01-06 21:39:44 -0700

    Added Calendar component, and deleted default code

---
+++ src/Calendar.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class Calendar extends Component {
+  render() {
+    return (
+      <div>
+      </div>
+    );
+  }
+}
+
+export default Calendar;
